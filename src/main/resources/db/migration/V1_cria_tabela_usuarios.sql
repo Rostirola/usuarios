4fdac54f5120c35226e3b3097eab1de6028a1cb1
Juan Rostirola <juanvitorrostirola666@gmail.com>
2024-03-05 22:19:08 -0300

    :sparkles: New feature

---
+++ src/main/resources/db/migration/V1_cria_tabela_usuarios.sql
@@ -0,0 +1,6 @@
+CREATE TABLE alimentos (
+                           id bigint(20) NOT NULL AUTO_INCREMENT,
+                           tipo varchar(255) NOT NULL,
+                           valor decimal(19,2) NOT NULL,
+                           PRIMARY KEY (id)
+);
